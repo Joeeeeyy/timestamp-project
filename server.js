85538d2936db2497721d541b6d4fb02c2e14ca92
Jose Lopez <joseglopez1289@gmail.com>
2021-02-04 17:56:50 -0800

    A

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+let cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -27,16 +27,16 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
 let dateObj = {}
 
-app.get('/api/timestamp/:date_string', (req, res) => {
-  let dateString = req.params.date_string
+app.get('/api/timestamp/:input', (req, res) => {
+  let input = req.params.input
 
-  if(dateString.includes('-')) {
+  if(input.includes('-')) {
     dateObj['unix'] = new Date(input).getTime()
   }
 
